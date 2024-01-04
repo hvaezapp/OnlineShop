0727da7315d55d245ef7106c3cfc54a092379887
hassa <hassa@LAPTOP-U03CN6LR>
2024-01-04 21:18:43 +0330

    size api fix

--- Src/Api/OnlineShop.Api/Controllers/v1/SizeController.cs
+++ Src/Api/OnlineShop.Api/Controllers/v1/SizeController.cs
@@ -1,9 +1,6 @@
 ﻿using MediatR;
 using Microsoft.AspNetCore.Mvc;
-using OnlineShop.Application.DTOs.Color;
 using OnlineShop.Application.DTOs.Size;
-using OnlineShop.Application.Features.Color.Requests.Commands;
-using OnlineShop.Application.Features.Color.Requests.Queries;
 using OnlineShop.Application.Features.Size.Requests.Commands;
 using OnlineShop.Application.Features.Size.Requests.Queries;
 
@@ -17,17 +14,14 @@ namespace OnlineShop.Api.Controllers.v1
         {
             _mediator = mediator;
         }
-
+        
 
         // GET: api/<SizeController>
         [HttpGet("[action]")]
         public async Task<IActionResult> GetAll(CancellationToken cancellationToken)
-        {
-
+        { 
             return Ok(await _mediator.Send(new GetSizeListRequest(), cancellationToken));
-
         }
-
 
 
         // POST api/<SizeController>
@@ -35,8 +29,8 @@ namespace OnlineShop.Api.Controllers.v1
         public async Task<IActionResult> Create(CreateSizeDto dto, CancellationToken cancellationToken)
         {
             return Ok(await _mediator.Send(new CreateSizeCommand { CreateSizeDto = dto }, cancellationToken));
-
         }
+
 
     }
 }
